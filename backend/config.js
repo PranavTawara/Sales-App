1b5c3ca08548ac94ac42aeae272fd6798e8c842f
Pranav <pranavmarch12@gmail.com>
2024-04-29 00:00:08 +0530

    updated

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    MONGOBD_URL: "mongodb://127.0.0.1:27017/assignment5",
+    MONGOBD_URL: "mongodb+srv://pranav:K0oe3FE2EhDWSWSd@cluster0.cnbpd3j.mongodb.net/salesapp?retryWrites=true&w=majority&appName=Cluster0",
     JWT_SECRET: "hafsfffgg72846285jjjkh54646467hh"
 }
